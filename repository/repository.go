eb302b5312a3ee77f0b193b9296812819b186930
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 15:59:34 -0300

    feat(subService/repository): move repo interface to postgres_repository.go

--- repository/repository.go
+++ repository/repository.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"subService/model"
 	"time"
 )
 
@@ -17,13 +16,4 @@ type SumFilter struct {
 	ServiceName string
 	From        time.Time
 	To          time.Time
-}
-
-type SubscriptionRepository interface {
-	Create(sub *model.Subscription) error
-	GetByID(id string) (*model.Subscription, error)
-	Update(sub *model.Subscription) error
-	Delete(id string) error
-	GetAll(filter SubscriptionFilter) ([]*model.Subscription, error)
-	GetSum(filter SumFilter) (int, error)
 }
