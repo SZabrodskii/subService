abc06f2a7e5188a25856c6c58ee02a0aa0fce602
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 14:36:15 -0300

    feat(subService/config): init config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"go.uber.org/fx"
+)
+
+type Config struct {
+	Port   int
+	DbUrl  string
+	AppEnv string
+}
+
+func Provide() fx.Option {
+	return fx.Provide(NewConfig)
+}
+
+func NewConfig() (*Config, error) {
+	v := viper.New()
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Println("No config.yaml loaded, using defaults and ENV:", err)
+	}
+	v.SetDefault("port", 8040)
+
+	cfg := &Config{
+		Port:   v.GetInt("PORT"),
+		DbUrl:  v.GetString("DB_URL"),
+		AppEnv: v.GetString("APP_ENV"),
+	}
+	if cfg.DbUrl == "" {
+		return nil, fmt.Errorf("DB_URL is required")
+	}
+
+	return cfg, nil
+}
