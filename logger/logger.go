e628ee6dfd1519274970ed9e675b2dd4e0d73921
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 14:21:43 -0300

    feat(subService/logger): init logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,29 @@
 package logger
+
+import (
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"os"
+)
+
+func NewZapLogger() *zap.Logger {
+	var logger *zap.Logger
+	var err error
+
+	if val, ok := os.LookupEnv("APP_ENV"); ok && val == "production" {
+		logger, err = zap.NewProduction()
+	} else {
+		logger, err = zap.NewDevelopment()
+	}
+
+	if err != nil {
+		panic("error creating zap logger" + err.Error())
+	}
+
+	return logger
+
+}
+
+func Provide() fx.Option {
+	return fx.Provide(NewZapLogger)
+}
