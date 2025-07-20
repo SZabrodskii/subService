8ff24bef5f314eaeda05102915c163b23af340d8
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 14:55:31 -0300

    feat(subService/model): init Subscription model

--- model/subscription.go
+++ model/subscription.go
@@ -1,1 +1,14 @@
 package model
+
+import "time"
+
+type Subscription struct {
+	ID          string    `gorm:"type:uuid;default:gen_random_uuid();primary_key" json:"id"`
+	ServiceName string    `dorm:"not null" json:"service_name"`
+	Price       int       `dorm:"not null" json:"price"`
+	UserID      string    `dorm:"type:uuid;not null" json:"user_id"`
+	StartDate   time.Time `dorm:"type:date;not null" json:"start_date"`
+	EndDate     time.Time `dorm:"type:date;not null" json:"end_date,omitempty"`
+	CreatedAt   time.Time `dorm:"type:autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time `dorm:"type:autoUpdateTime" json:"updated_at"`
+}
