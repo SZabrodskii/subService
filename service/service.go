91ba25039e8d20abfc5bf441e76fc669c4652482
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 16:00:08 -0300

    feat(subService/service): add create and update structs

--- service/service.go
+++ service/service.go
@@ -1,1 +1,15 @@
 package service
+
+type CreateSubscriptionRequest struct {
+	ServiceName string  `json:"service_name" binding:"required"`
+	Price       int     `json:"price" binding:"required,min=0"`
+	UserID      string  `json:"user_id" binding:"required,uuid"`
+	StartDate   string  `json:"start_date" binding:"required,datetime=2006-01"`
+	EndDate     *string `json:"end_date,omitempty" binding:"omitempty,datetime=2006-01"`
+}
+
+type UpdateSubscriptionRequest struct {
+	ServiceName *string `json:"service_name,omitempty" binding:"omitempty,required_with=Price"`
+	Price       *int    `json:"price,omitempty" binding:"omitempty,min=0"`
+	EndDate     *string `json:"end_date" binding:"omitempty,datetime=2006-01"`
+}
