1ce7668034a46f8a60a4df1be8dd79cb15d5892e
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 14:55:52 -0300

    feat(subService/repository): init Subscription repo params

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,29 @@
 package repository
+
+import (
+	"subService/model"
+	"time"
+)
+
+type SubscriptionFilter struct {
+	UserID      string
+	ServiceName string
+	From        time.Time
+	To          time.Time
+}
+
+type SumFilter struct {
+	UserID      string
+	ServiceName string
+	From        time.Time
+	To          time.Time
+}
+
+type SubscriptionRepository interface {
+	Create(sub *model.Subscription) error
+	GetByID(id string) (*model.Subscription, error)
+	Update(sub *model.Subscription) error
+	Delete(id string) error
+	GetAll(filter SubscriptionFilter) ([]*model.Subscription, error)
+	GetSum(filter SumFilter) (int, error)
+}
