5efc98ce4f83a697eed3e190ba8c83fea10799b8
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-07-20 16:45:26 -0300

    feat(subService/model): add SubscriptionResponse struct and its method ToResponse()

--- model/subscription.go
+++ model/subscription.go
@@ -5,10 +5,40 @@ import "time"
 type Subscription struct {
 	ID          string     `gorm:"type:uuid;default:gen_random_uuid();primary_key" json:"id"`
 	ServiceName string     `dorm:"not null" json:"service_name"`
-	Price       int        `dorm:"not null" json:"price"`
-	UserID      string     `dorm:"type:uuid;not null" json:"user_id"`
-	StartDate   time.Time  `dorm:"type:date;not null" json:"start_date"`
-	EndDate     *time.Time `dorm:"type:date" json:"end_date,omitempty"`
-	CreatedAt   time.Time  `dorm:"type:autoCreateTime" json:"created_at"`
-	UpdatedAt   time.Time  `dorm:"type:autoUpdateTime" json:"updated_at"`
+	Price       int        `gorm:"not null" json:"price"`
+	UserID      string     `gorm:"type:uuid;not null" json:"user_id"`
+	StartDate   time.Time  `gorm:"type:date;not null" json:"start_date"`
+	EndDate     *time.Time `gorm:"type:date" json:"end_date,omitempty"`
+	CreatedAt   time.Time  `gorm:"type:autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time  `gorm:"type:autoUpdateTime" json:"updated_at"`
+}
+
+type SubscriptionResponse struct {
+	ID          string  `json:"id"`
+	ServiceName string  `json:"service_name"`
+	Price       int     `json:"price"`
+	UserID      string  `json:"user_id"`
+	StartDate   string  `json:"start_date"`
+	EndDate     *string `json:"end_date,omitempty"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
+}
+
+func (s *Subscription) ToResponse() SubscriptionResponse {
+	start := s.StartDate.Format("2006-01")
+	var end *string
+	if s.EndDate != nil {
+		e := s.EndDate.Format("2006-01")
+		end = &e
+	}
+	return SubscriptionResponse{
+		ID:          s.ID,
+		ServiceName: s.ServiceName,
+		Price:       s.Price,
+		UserID:      s.UserID,
+		StartDate:   start,
+		EndDate:     end,
+		CreatedAt:   s.CreatedAt.Format(time.RFC3339),
+		UpdatedAt:   s.UpdatedAt.Format(time.RFC3339),
+	}
 }
